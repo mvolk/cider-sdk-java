0c9538628f65d7d6a510af105ea10f7eab80fd07
Michael Volk <michael@volksys.com>
2016-05-15 20:39:49 -0700

    Add specific gravity to the CiderRef SDK model.

---
+++ src/main/java/com/ciderref/sdk/property/SpecificGravity.java
@@ -0,0 +1,158 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Michael Volk (michael@volksys.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.ciderref.sdk.property;
+
+import com.ciderref.sdk.substance.Water;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+/**
+ * Represents the ratio of the density of some liquid (typically apple juice, must or hard cider in this context) to
+ * the density of pure water. This is a dimensionless value. Immutable and thread-safe.
+ */
+public class SpecificGravity implements Comparable<SpecificGravity> {
+
+    /** The lowest specific gravity that the CiderRef SDK supports. */
+    public static final double MINIMUM_SUPPORTED_VALUE = 0.990;
+
+    /** The highest specific gravity that the CiderRef SDK supports. */
+    public static final double MAXIMUM_SUPPORTED_VALUE = 1.100;
+
+    private static final NumberFormat FORMAT = new DecimalFormat("0.000");
+
+    private final double value;
+
+    /**
+     * Constructor for values that are already corrected for temperature.
+     *
+     * @param actualSpecificGravity actual specific gravity, after correcting for temperature
+     * @throws IllegalPropertyValueException if the {@code value} is less than {@link #MINIMUM_SUPPORTED_VALUE} or
+     *         exceeds {@link #MAXIMUM_SUPPORTED_VALUE}.
+     */
+    public SpecificGravity(double actualSpecificGravity) {
+        checkSpecificGravityValidity(actualSpecificGravity, "specific gravity");
+        this.value = actualSpecificGravity;
+    }
+
+    /**
+     * Constructor for values that are not already corrected for temperature.
+     *
+     * @param measuredSpecificGravity measured specific gravity of the fluid, before correcting for temperature
+     * @param measuredTemperature (not null) measured temperature of the fluid
+     * @param calibrationTemperature (not null) the calibration temperature of the hydrometer used to measure the
+     *                               specific gravity
+     * @throws IllegalArgumentException if {@code measuredTemperature} or {@code calibrationTemperature} is
+     *         {@code null}.
+     * @throws IllegalPropertyValueException if the {@code measuredTemperature} or {@code calibrationTemperature} is
+     *         not between {@link Water#STANDARD_FREEZING_POINT} and {@link Water#STANDARD_BOILING_POINT}, inclusive.
+     * @throws IllegalPropertyValueException if the measured or corrected specific gravity is not between
+     *         {@link #MINIMUM_SUPPORTED_VALUE} and {@link #MAXIMUM_SUPPORTED_VALUE}, inclusive.
+     */
+    public SpecificGravity(double measuredSpecificGravity,
+                           Temperature measuredTemperature,
+                           Temperature calibrationTemperature) {
+
+        checkSpecificGravityValidity(measuredSpecificGravity, "measured specific gravity");
+
+        Water water = new Water();
+        double correctionFactor = water.getDensity(calibrationTemperature) / water.getDensity(measuredTemperature);
+        this.value = correctionFactor * measuredSpecificGravity;
+
+        checkSpecificGravityValidity(this.value, "corrected specific gravity");
+    }
+
+    private void checkSpecificGravityValidity(double specificGravity, String name) {
+        if (specificGravity < MINIMUM_SUPPORTED_VALUE || specificGravity > MAXIMUM_SUPPORTED_VALUE) {
+            throw new IllegalPropertyValueException("The " + name + ", " + FORMAT.format(specificGravity)
+                    + ", is not within the range of supported specific gravity values. Specific gravities between "
+                    + FORMAT.format(MINIMUM_SUPPORTED_VALUE) + " and " + FORMAT.format(MAXIMUM_SUPPORTED_VALUE)
+                    + ", inclusive, are supported.");
+        }
+    }
+
+    /**
+     * The actual (corrected) specific gravity.
+     *
+     * @return actual specific gravity.
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Compares one specific gravity to another in order from least dense to most dense.
+     *
+     * @param otherSpecificGravity the other specificGravity
+     * @return the value {@code 0} if this specific gravity is the same as {@code otherSpecificGravity}; a value less
+     *         than {@code 0} if this specific gravity is lower than {@code otherSpecificGravity}; and a value greater
+     *         than {@code 0} if this specific gravity is higher than {@code otherSpecificGravity}.
+     *
+     * @throws NullPointerException if {@code otherSpecificGravity} is null
+     */
+    @Override
+    public int compareTo(SpecificGravity otherSpecificGravity) {
+        return otherSpecificGravity == null ? 1 : Double.compare(value, otherSpecificGravity.value);
+    }
+
+    /**
+     * Determines whether this object is "equal" to another object.
+     *
+     * <p>This object is "equal" to another object if and only if:
+     * <ul>
+     *     <li>The other object is not null</li>
+     *     <li>The other object is an {code instanceof} this class</li>
+     *     <li>This object {@link #compareTo(SpecificGravity)} the other object returns {@code 0} (equivalent)</li>
+     * </ul>
+     *
+     * @param other (nullable) the object to compare with this object
+     * @return true if the two objects are "equal" according to the criteria above.
+     */
+    @Override
+    public final boolean equals(Object other) {
+        return other instanceof SpecificGravity && (other == this || this.compareTo((SpecificGravity) other) == 0);
+    }
+
+    /**
+     * A hash code for this specific gravity.
+     *
+     * @return a hash code value for this object
+     */
+    @Override
+    public final int hashCode() {
+        return ((Double) value).hashCode();
+    }
+
+    /**
+     * The standard "0.000"-format representation of this specific gravity.
+     *
+     * @return (not null) textual representation of this specific gravity
+     */
+    @Override
+    public String toString() {
+        return FORMAT.format(value);
+    }
+
+}
---
+++ src/test/java/com/ciderref/sdk/property/SpecificGravityTest.java
@@ -0,0 +1,285 @@
+package com.ciderref.sdk.property;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import com.ciderref.sdk.substance.Water;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.Test;
+
+/**
+ * Units tests for {@link SpecificGravity}. Expected corrected values are drawn from The New Ciderist's Handbook by
+ * Claude Jolicoeur, published in 2013 by Chelsea Green Publishing in White River Junction, Vermont.
+ */
+@SuppressWarnings({"PMD.TooManyMethods", "PMD.ExcessivePublicCount"})
+public class SpecificGravityTest {
+
+    private static final Temperature FREEZING = Water.STANDARD_FREEZING_POINT;
+    private static final Temperature ROOM_TEMPERATURE = new Temperature(68, TemperatureUnits.Fahrenheit);
+    private static final Temperature BOILING = Water.STANDARD_BOILING_POINT;
+
+    /** Constructor throws if the specific gravity is too low. */
+    @Test(expected = IllegalPropertyValueException.class)
+    public void testConstructorValueTooLow() {
+        new SpecificGravity(SpecificGravity.MINIMUM_SUPPORTED_VALUE - 0.01);
+    }
+
+    /** Constructor throws if the specific gravity is too high. */
+    @Test(expected = IllegalPropertyValueException.class)
+    public void testConstructorValueTooHigh() {
+        new SpecificGravity(SpecificGravity.MAXIMUM_SUPPORTED_VALUE + 0.01);
+    }
+
+    /** Constructor correctly handles the minimum supported value. */
+    @Test
+    public void testConstructorValueLowLimit() {
+        assertEquals(SpecificGravity.MINIMUM_SUPPORTED_VALUE,
+                new SpecificGravity(SpecificGravity.MINIMUM_SUPPORTED_VALUE).getValue(), 0);
+    }
+
+    /** Constructor correctly handles the maximum supported value. */
+    @Test
+    public void testConstructorValueHighLimit() {
+        assertEquals(SpecificGravity.MAXIMUM_SUPPORTED_VALUE,
+                new SpecificGravity(SpecificGravity.MAXIMUM_SUPPORTED_VALUE).getValue(), 0);
+    }
+
+    /** Constructor correctly handles a value midway between the minimum and maximum supported values. */
+    @Test
+    public void testConstructorValueMiddleBoundary() {
+        double middleValue = (SpecificGravity.MINIMUM_SUPPORTED_VALUE + SpecificGravity.MAXIMUM_SUPPORTED_VALUE) / 2;
+        assertEquals(middleValue, new SpecificGravity(middleValue).getValue(), 0);
+    }
+
+    /** Correcting constructor throws if the measured specific gravity is too low. */
+    @Test(expected = IllegalPropertyValueException.class)
+    public void testCorrectingConstructorMeasuredValueTooLow() {
+        new SpecificGravity(SpecificGravity.MINIMUM_SUPPORTED_VALUE - 0.01, ROOM_TEMPERATURE, ROOM_TEMPERATURE);
+    }
+
+    /** Correcting constructor throws if the measured specific gravity is too high. */
+    @Test(expected = IllegalPropertyValueException.class)
+    public void testCorrectingConstructorMeasuredValueTooHigh() {
+        new SpecificGravity(SpecificGravity.MAXIMUM_SUPPORTED_VALUE + 0.01, ROOM_TEMPERATURE, ROOM_TEMPERATURE);
+    }
+
+    /** Correcting constructor throws if the corrected specific gravity is too low. */
+    @Test
+    public void testCorrectingConstructorCorrectedValueTooLow() {
+        try {
+            new SpecificGravity(SpecificGravity.MINIMUM_SUPPORTED_VALUE, ROOM_TEMPERATURE, BOILING);
+            fail("Expected IllegalPropertyValueException");
+        } catch (IllegalPropertyValueException ex) {
+            assertTrue(ex.getMessage().contains("corrected specific gravity"));
+        }
+    }
+
+    /** Correcting constructor throws if the corrected specific gravity is too high. */
+    @Test
+    public void testCorrectingConstructorCorrectedValueTooHigh() {
+        try {
+            new SpecificGravity(SpecificGravity.MAXIMUM_SUPPORTED_VALUE, ROOM_TEMPERATURE, FREEZING);
+            fail("Expected IllegalPropertyValueException");
+        } catch (IllegalPropertyValueException ex) {
+            assertTrue(ex.getMessage().contains("corrected specific gravity"));
+        }
+    }
+
+    /**
+     * Correcting constructor correctly handles the minimum supported value when measured at the hydrometer's
+     * calibration temperature.
+     */
+    @Test
+    public void testCorrectingConstructorValueLowLimit() {
+        SpecificGravity sg =
+                new SpecificGravity(SpecificGravity.MINIMUM_SUPPORTED_VALUE, ROOM_TEMPERATURE, ROOM_TEMPERATURE);
+        assertEquals(SpecificGravity.MINIMUM_SUPPORTED_VALUE, sg.getValue(), 0);
+    }
+
+    /**
+     * Correcting constructor correctly handles the maximum supported value when measured at the hydrometer's
+     * calibration temperature.
+     */
+    @Test
+    public void testCorrectingConstructorValueHighLimit() {
+        SpecificGravity sg =
+                new SpecificGravity(SpecificGravity.MAXIMUM_SUPPORTED_VALUE, ROOM_TEMPERATURE, ROOM_TEMPERATURE);
+        assertEquals(SpecificGravity.MAXIMUM_SUPPORTED_VALUE, sg.getValue(), 0);
+    }
+
+    /**
+     * Correcting constructor correctly handles a value midway between the low and high limits that is measured
+     * at the hydrometer's calibration temperature.
+     */
+    @Test
+    public void testCorrectingConstructorValueMiddleBoundary() {
+        double middleValue = (SpecificGravity.MINIMUM_SUPPORTED_VALUE + SpecificGravity.MAXIMUM_SUPPORTED_VALUE) / 2;
+        assertEquals(middleValue, new SpecificGravity(middleValue, ROOM_TEMPERATURE, ROOM_TEMPERATURE).getValue(), 0);
+    }
+
+    /** Correcting constructor throws if the measured temperature is too high. */
+    @Test(expected = IllegalPropertyValueException.class)
+    public void testCorrectingConstructorMeasuredTemperatureTooHigh() {
+        new SpecificGravity(1.0, new Temperature(100.01, TemperatureUnits.Celsius), ROOM_TEMPERATURE);
+    }
+
+    /** Correcting constructor throws if the measured temperature is too low. */
+    @Test(expected = IllegalPropertyValueException.class)
+    public void testCorrectingConstructorMeasuredTemperatureTooLow() {
+        new SpecificGravity(1.0, new Temperature(-0.01, TemperatureUnits.Celsius), ROOM_TEMPERATURE);
+    }
+
+    /** Correcting constructor accepts a measured temperature at the maximum supported temperature. */
+    @Test
+    public void testCorrectingConstructorMeasuredTemperatureAtHighLimit() {
+        SpecificGravity sg = new SpecificGravity(1.0, BOILING, ROOM_TEMPERATURE);
+        assertEquals(1.04135, sg.getValue(), 0.0001);
+    }
+
+    /** Correcting constructor accepts a measured temperature at the minimum supported temperature. */
+    @Test
+    public void testCorrectingConstructorMeasuredTemperatureAtLowLimit() {
+        SpecificGravity sg =  new SpecificGravity(1.0, FREEZING, ROOM_TEMPERATURE);
+        assertEquals(0.99836, sg.getValue(), 0.00001);
+    }
+
+    /** Correcting constructor throws if the measured temperature is null. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testCorrectingConstructorNullMeasuredTemperature() {
+        new SpecificGravity(1.0, null, ROOM_TEMPERATURE);
+    }
+
+    /** Correcting constructor throws if the calibration temperature is too high. */
+    @Test(expected = IllegalPropertyValueException.class)
+    public void testCorrectingConstructorCalibrationTemperatureTooHigh() {
+        new SpecificGravity(1.0, ROOM_TEMPERATURE, new Temperature(100.01, TemperatureUnits.Celsius));
+    }
+
+    /** Correcting constructor throws if the calibration temperature is too low. */
+    @Test(expected = IllegalPropertyValueException.class)
+    public void testCorrectingConstructorCalibrationTemperatureTooLow() {
+        new SpecificGravity(1.0, ROOM_TEMPERATURE, new Temperature(-0.01, TemperatureUnits.Celsius));
+    }
+
+    /** Correcting constructor accepts a calibration temperature at the maximum limit. */
+    @Test
+    public void testCorrectingConstructorCalibrationTemperatureAtHighLimit() {
+        SpecificGravity sg = new SpecificGravity(1.1, ROOM_TEMPERATURE, BOILING);
+        assertEquals(1.05632, sg.getValue(), 0.00001);
+    }
+
+    /** Correcting constructor accepts a calibration temperature at the minimum limit. */
+    @Test
+    public void testCorrectingConstructorCalibrationTemperatureAtLowLimit() {
+        SpecificGravity sg = new SpecificGravity(1.0, ROOM_TEMPERATURE, FREEZING);
+        assertEquals(1.00163, sg.getValue(), 0.00001);
+    }
+
+    /** Correcting constructor throws when passed a null calibration temperature. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testCorrectingConstructorNullCalibrationTemperature() {
+        new SpecificGravity(1.0, ROOM_TEMPERATURE, null);
+    }
+
+    /** Correcting constructor correctly corrects 1.020 at 43℉ with hydrometer calibrated at 60℉. */
+    @Test
+    public void testCorrectingConstructorCorrectAt43Fahrenheit() {
+        Temperature measuredTemperature = new Temperature(43, TemperatureUnits.Fahrenheit);
+        Temperature calibrationTemperature = new Temperature(60, TemperatureUnits.Fahrenheit);
+        SpecificGravity correctedSg = new SpecificGravity(1.020, measuredTemperature, calibrationTemperature);
+        assertEquals(1.0190, correctedSg.getValue(), 0.0001);
+    }
+
+    /** Correcting constructor correctly corrects 1.020 at 50℉ with hydrometer calibrated at 60℉. */
+    @Test
+    public void testCorrectingConstructorCorrectAt50Fahrenheit() {
+        Temperature measuredTemperature = new Temperature(50, TemperatureUnits.Fahrenheit);
+        Temperature calibrationTemperature = new Temperature(60, TemperatureUnits.Fahrenheit);
+        SpecificGravity correctedSg = new SpecificGravity(1.020, measuredTemperature, calibrationTemperature);
+        assertEquals(1.0193, correctedSg.getValue(), 0.0001);
+    }
+
+    /** Correcting constructor correctly corrects 1.020 at 60℉ with hydrometer calibrated at 60℉. */
+    @Test
+    public void testCorrectingConstructorCorrectAt60Fahrenheit() {
+        Temperature measuredTemperature = new Temperature(60, TemperatureUnits.Fahrenheit);
+        Temperature calibrationTemperature = new Temperature(60, TemperatureUnits.Fahrenheit);
+        SpecificGravity correctedSg = new SpecificGravity(1.020, measuredTemperature, calibrationTemperature);
+        assertEquals(1.020, correctedSg.getValue(), 0.0001);
+    }
+
+    /** Correcting constructor correctly corrects 1.020 at 65℉ with hydrometer calibrated at 60℉. */
+    @Test
+    public void testCorrectingConstructorCorrectAt65Fahrenheit() {
+        Temperature measuredTemperature = new Temperature(65, TemperatureUnits.Fahrenheit);
+        Temperature calibrationTemperature = new Temperature(60, TemperatureUnits.Fahrenheit);
+        SpecificGravity correctedSg = new SpecificGravity(1.020, measuredTemperature, calibrationTemperature);
+        assertEquals(1.0205, correctedSg.getValue(), 0.0001);
+    }
+
+    /** Correcting constructor correctly corrects 1.020 at 70℉ with hydrometer calibrated at 60℉. */
+    @Test
+    public void testCorrectingConstructorCorrectAt70Fahrenheit() {
+        Temperature measuredTemperature = new Temperature(70, TemperatureUnits.Fahrenheit);
+        Temperature calibrationTemperature = new Temperature(60, TemperatureUnits.Fahrenheit);
+        SpecificGravity correctedSg = new SpecificGravity(1.020, measuredTemperature, calibrationTemperature);
+        assertEquals(1.021, correctedSg.getValue(), 0.0001);
+    }
+
+    /** Correcting constructor correctly corrects 1.020 at 77℉ with hydrometer calibrated at 60℉. */
+    @Test
+    public void testCorrectingConstructorCorrectAt77Fahrenheit() {
+        Temperature measuredTemperature = new Temperature(77, TemperatureUnits.Fahrenheit);
+        Temperature calibrationTemperature = new Temperature(60, TemperatureUnits.Fahrenheit);
+        SpecificGravity correctedSg = new SpecificGravity(1.020, measuredTemperature, calibrationTemperature);
+        assertEquals(1.022, correctedSg.getValue(), 0.0001);
+    }
+
+    /** Correcting constructor correctly corrects 1.020 at 84℉ with hydrometer calibrated at 60℉. */
+    @Test
+    public void testCorrectingConstructorCorrectAt84Fahrenheit() {
+        Temperature measuredTemperature = new Temperature(84, TemperatureUnits.Fahrenheit);
+        Temperature calibrationTemperature = new Temperature(60, TemperatureUnits.Fahrenheit);
+        SpecificGravity correctedSg = new SpecificGravity(1.020, measuredTemperature, calibrationTemperature);
+        assertEquals(1.023, correctedSg.getValue(), 0.0001);
+    }
+
+    /** 1.020 is the same as 1.020 */
+    @Test
+    public void testCompareToSameValue() {
+        assertEquals(0, new SpecificGravity(1.020).compareTo(new SpecificGravity(1.020)));
+    }
+
+    /** 1.020 is more than 1.010 */
+    @Test
+    public void testCompareToSmallerValue() {
+        assertEquals(1, new SpecificGravity(1.020).compareTo(new SpecificGravity(1.010)));
+    }
+
+    /** 1.020 is more than 1.030 */
+    @Test
+    public void testCompareToLargerValue() {
+        assertEquals(-1, new SpecificGravity(1.020).compareTo(new SpecificGravity(1.030)));
+    }
+
+    /** Java .equals(Object) contract is met. */
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    public void testEqualsContract() {
+        EqualsVerifier.forClass(SpecificGravity.class).verify();
+    }
+
+    /** toString correctly represents specific gravities higher than 1. */
+    @Test
+    public void testToStringAbove1() {
+        assertEquals("1.010", new SpecificGravity(1.01).toString());
+    }
+
+    /** toString correctly represents specific gravities less than 1. */
+    @Test
+    public void testToStringBelow1() {
+        assertEquals("0.990", new SpecificGravity(.99).toString());
+    }
+
+}
