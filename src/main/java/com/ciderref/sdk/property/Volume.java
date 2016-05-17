977face4638104f12cf24c7166852365dcaba5a0
Michael Volk <michael@volksys.com>
2016-05-16 21:36:31 -0700

    Add volume to the CiderRef SDK model.

---
+++ src/main/java/com/ciderref/sdk/property/Volume.java
@@ -0,0 +1,131 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Michael Volk (michael@volksys.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.ciderref.sdk.property;
+
+/**
+ * Represents a volume. Immutable and thread-safe.
+ */
+public class Volume implements Comparable<Volume> {
+
+    public enum Units {
+        Milliliters, Liters, USGallons
+    }
+
+    private final double milliliters;
+
+    /**
+     * Constructor.
+     *
+     * @param value the size of the volume.
+     * @param units (not null) the units of measurement in which the size of the volume is expressed.
+     *
+     * @throws IllegalArgumentException if {@code units} is {@code null}
+     */
+    public Volume(double value, Units units) {
+        if (units == null) {
+            throw new IllegalArgumentException("Volume cannot be represented without units of measurement.");
+        }
+        switch (units) {
+            case Liters:
+                this.milliliters = value * 1000.0;
+                break;
+            case USGallons:
+                this.milliliters = value * 3785.411784;
+                break;
+            default: // Milliliters
+                this.milliliters = value;
+                break;
+        }
+    }
+
+    /**
+     * This volume expressed in a specific unit of measurement.
+     *
+     * @param units (not null) the unit of measurement in which to return this volume
+     * @return this volume expressed in the given unit of measurement
+     *
+     * @throws IllegalArgumentException if {@code units} is null
+     */
+    public double getValue(Units units) {
+        if (units == null) {
+            throw new IllegalArgumentException("Volume cannot be represented without units of measurement.");
+        }
+        switch (units) {
+            case Liters:
+                return milliliters / 1000.0;
+            case USGallons:
+                return milliliters / 3785.411784;
+            default: // Milliliters
+                return milliliters;
+        }
+    }
+
+    /**
+     * Compares this volume to another volume. Note that values that are within 1/100th of a milliliter of each other
+     * are considered equivalent.
+     *
+     * @param otherVolume the other volume
+     * @return {@code true} if this volume is larger than {@code otherVolume}; {@code false} otherwise.
+     *
+     * @throws NullPointerException if {@code otherVolume} is null
+     */
+    @Override
+    public int compareTo(Volume otherVolume) {
+        if (Math.abs(milliliters - otherVolume.milliliters) <= 0.01) {
+            return 0;
+        } else {
+            return Double.compare(milliliters, otherVolume.milliliters);
+        }
+    }
+
+    /**
+     * Determines whether this object is "equal" to another object.
+     *
+     * <p>This object is "equal" to another object if and only if:
+     * <ul>
+     *     <li>The other object is not {@code null}</li>
+     *     <li>The other object is an {@code instanceof} this class</li>
+     *     <li>This object {@link #compareTo(Volume)} the other object returns {@code 0} (equivalent)</li>
+     * </ul>
+     *
+     * @param other (nullable) the object to compare with this object
+     * @return true if the two objects are "equal" according to the criteria above.
+     */
+    @Override
+    public final boolean equals(Object other) {
+        return other instanceof Volume && (other == this || this.compareTo((Volume) other) == 0);
+    }
+
+    /**
+     * A units-agnostic hash code for this temperature.
+     *
+     * @return a hash code value for this object
+     */
+    @Override
+    public final int hashCode() {
+        return ((Double) milliliters).hashCode();
+    }
+
+}
---
+++ src/test/java/com/ciderref/sdk/property/VolumeTest.java
@@ -0,0 +1,212 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Michael Volk (michael@volksys.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.ciderref.sdk.property;
+
+import static com.ciderref.sdk.property.Volume.Units.Liters;
+import static com.ciderref.sdk.property.Volume.Units.Milliliters;
+import static com.ciderref.sdk.property.Volume.Units.USGallons;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link Volume}.
+ */
+@SuppressWarnings({"PMD.TooManyMethods", "PMD.ExcessivePublicCount", "PMD.TooManyStaticImports"})
+public class VolumeTest {
+
+    /** Volume.Units.values() should return 3 values. */
+    @Test
+    public void testCountOfUnitValues() {
+        assertEquals(3, Volume.Units.values().length);
+    }
+
+    /** Volume.Units.valueOf("Milliliters") should return Milliliters. */
+    @Test
+    public void testUnitsIncludeMilliliters() {
+        assertEquals(Milliliters, Volume.Units.valueOf("Milliliters"));
+    }
+
+    /** Volume.Units.valueOf("Liters") should return Liters. */
+    @Test
+    public void testUnitsIncludeLiters() {
+        assertEquals(Liters, Volume.Units.valueOf("Liters"));
+    }
+
+    /** Volume.Units.valueOf("USGallons") should return USGallons. */
+    @Test
+    public void testUnitsIncludeUsGallons() {
+        assertEquals(USGallons, Volume.Units.valueOf("USGallons"));
+    }
+
+    /** Constructing with null units of measurement produces an exception. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorNullUnitsThrows() {
+        new Volume(5, null);
+    }
+
+    /** Constructor handles milliliters correctly. */
+    @Test
+    public void testConstructorMilliliters() {
+        assertEquals(5, new Volume(5, Milliliters).getValue(Milliliters), 0);
+    }
+
+    /** Constructor handles liters correctly. */
+    @Test
+    public void testConstructorLiters() {
+        assertEquals(5, new Volume(5, Liters).getValue(Liters), 0);
+    }
+
+    /** Constructor handles US Gallons correctly. */
+    @Test
+    public void testConstructorUsGallons() {
+        assertEquals(5, new Volume(5, USGallons).getValue(USGallons), 0);
+    }
+
+    /** getValue throws if null units passed in. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetValueWithNullArgumentThrows() {
+        new Volume(5000, Milliliters).getValue(null);
+    }
+
+    /** Converts Milliliters to Liters correctly. */
+    @Test
+    public void testGetMillilitersAsLiters() {
+        assertEquals(5, new Volume(5000, Milliliters).getValue(Liters), 0);
+    }
+
+    /** Converts Milliliters to US Gallons correctly. */
+    @Test
+    public void testGetMillilitersAsUsGallons() {
+        assertEquals(1.32086, new Volume(5000, Milliliters).getValue(USGallons), 0.000001);
+    }
+
+    /** Converts Liters to Milliliters correctly. */
+    @Test
+    public void testGetLitersAsMilliliters() {
+        assertEquals(5000, new Volume(5, Liters).getValue(Milliliters), 0);
+    }
+
+    /** Converts Liters to US Gallons correctly. */
+    @Test
+    public void testGetLitersAsUsGallons() {
+        assertEquals(1.32086, new Volume(5, Liters).getValue(USGallons), 0.000001);
+    }
+
+    /** Converts US Gallons to Milliliters correctly. */
+    @Test
+    public void testGetUsGallonsAsMilliliters() {
+        assertEquals(5000, new Volume(1.32086, USGallons).getValue(Milliliters), 0.01);
+    }
+
+    /** Converts Liters to US Gallons correctly. */
+    @Test
+    public void testGetUsGallonsAsLiters() {
+        assertEquals(5, new Volume(1.32086, USGallons).getValue(Liters), 0.000001);
+    }
+
+    /** Comparison to null yields a NPE. */
+    @Test(expected = NullPointerException.class)
+    public void testCompareToNullThrows() {
+        //noinspection ConstantConditions
+        new Volume(50, Liters).compareTo(null);
+    }
+
+    /** 5L is more than 1 US Gallon. */
+    @Test
+    public void testCompare5LitersTo1UsGallon() {
+        assertEquals(1, new Volume(5, Liters).compareTo(new Volume(1, USGallons)));
+    }
+
+    /** 1 US Gallon is less than 5 Liters. */
+    @Test
+    public void testCompare1UsGallonTo5Liters() {
+        assertEquals(-1, new Volume(1, USGallons).compareTo(new Volume(5, Liters)));
+    }
+
+    /** 1 Liter is 1000 Milliliters. */
+    @Test
+    public void testCompare1LiterTo1000Milliliters() {
+        assertEquals(0, new Volume(1, Liters).compareTo(new Volume(1000, Milliliters)));
+    }
+
+    /** 1000 Milliliters is 1 Liter. */
+    @Test
+    public void testCompare1000MillilitersTo1Liter() {
+        assertEquals(0, new Volume(1000, Milliliters).compareTo(new Volume(1, Liters)));
+    }
+
+    /** 20 L is not equal to null. */
+    @Test
+    public void testVolumeIsNotEqualToNull() {
+        //noinspection ObjectEqualsNull
+        assertFalse(new Volume(20, Liters).equals(null)); // NOPMD - equals(null) is the point of this test
+    }
+
+    /** 50L is not equal to "50L". */
+    @Test
+    public void testVolumeIsNotEqualToString() {
+        //noinspection EqualsBetweenInconvertibleTypes
+        assertFalse(new Volume(50, Liters).equals("50L"));
+    }
+
+    /** 0L is equal to 0 US Gallons. */
+    @Test
+    public void test0Equals0() {
+        assertTrue(new Volume(0, Liters).equals(new Volume(0, USGallons)));
+    }
+
+    /** 7L is not equal to 7mL. */
+    @Test
+    public void test7LitersIsNotEqualTo7Milliliters() {
+        assertFalse(new Volume(7, Liters).equals(new Volume(7, Milliliters)));
+    }
+
+    /** 1L is equal to 1000mL. */
+    @Test
+    public void test1LiterIsEqualTo1000Milliliters() {
+        assertTrue(new Volume(1, Liters).equals(new Volume(1000, Milliliters)));
+    }
+
+    /** Volume equals itself. */
+    @Test
+    public void testVolumeEqualsItself() {
+        Volume volume = new Volume(2, Milliliters);
+        @SuppressWarnings("UnnecessaryLocalVariable")
+        Volume itself = volume;
+        assertTrue(volume.equals(itself)); // NOPMD - exercising .equals(Object) is the point of this test
+    }
+
+    /** Java .equals(Object) contract is met. */
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    public void equalsContract() {
+        EqualsVerifier.forClass(Volume.class).verify();
+    }
+
+}
